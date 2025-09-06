5496803d43c38d0b721e00ffa504159abec8a8b0
Khushi Lohia <162160349+khushilohia@users.noreply.github.com>
2025-09-06 20:14:53 +0530

    light

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
-model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+tokenizer = AutoTokenizer.from_pretrained("t5-small")
+model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
 
 text = input("Enter the text to summarize: ")
 
